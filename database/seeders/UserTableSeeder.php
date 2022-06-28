fc161b3534555abf6f249f96d31f7cabb64fcb71
知念英弥 <s19013@std.it-college.ac.jp>
2022-06-28 23:31:05 +0900

    シーダー作成

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //ダミーデータ
+        DB::table('users')->insert([
+            [
+                'name' => 'test1',
+                'email' => 'test1@test',
+                'password' => bcrypt('test'),
+            ],
+                'name' => 'test2',
+                'email' => 'test2@test',
+                'password' => bcrypt('test'),
+        ]);
+    }
+}
